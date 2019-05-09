38cb88272e050b27a1fa0a567d661cf2c237693c
Jocelyn Lee <47438128+JJace421@users.noreply.github.com>
2019-05-09 10:39:28 -0400

    Create Environmental Design.py

---
+++ Environmental Design.py
@@ -0,0 +1,24 @@
+#Social isolation
+#chat box opened 
+
+places = ['Tim Hortions', 'Mcdonald','Park','Starbucks','Mall']
+
+print("Hello. Would you like to meet up?")
+answer = input()
+
+if answer == "yes":
+  while True:
+    time = int(input("Input time: "))
+    print(f"Would you like to meet at {time} o'clock ")
+    meet = input()
+    if meet == "yes":
+      print(f"Here are the available places: {places}. Where would you want to meet?")
+      meeting_place = input()     
+      print (f"I will see you at the {meeting_place}.")
+      break
+    else:
+      "Would you like to choose a different time?"
+else:
+  print("Sorry, I don't have the time.")
+  print("Goodbye.")
+  
