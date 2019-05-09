1cb2d13b4b35b6c239da1e2b7fccf27aebf9dafb
Jocelyn Lee <47438128+JJace421@users.noreply.github.com>
2019-05-09 10:46:55 -0400

    Update Environmental Design.py

--- Environmental Design.py
+++ Environmental Design.py
@@ -7,18 +7,18 @@ print("Hello. Would you like to meet up?")
 answer = input()
 
 if answer == "yes":
-  while True:
-    time = int(input("Input time: "))
-    print(f"Would you like to meet at {time} o'clock ")
-    meet = input()
-    if meet == "yes":
-      print(f"Here are the available places: {places}. Where would you want to meet?")
-      meeting_place = input()     
-      print (f"I will see you at the {meeting_place}.")
-      break
-    else:
-      "Would you like to choose a different time?"
+    while True: #continues to loop until a time is agreed to force the people to meet 
+        time = int(input("Input time: "))
+        print(f"Would you like to meet at {time} o'clock?")
+        meet = input()
+        if meet == "yes":
+            print(f"Here are the available places: {places}. Where would you want to meet?")
+            meeting_place = input()     
+            print (f"I will see you at the {meeting_place}.")
+            break
+        else:
+        print("Would you like to choose a different time?")
 else:
-  print("Sorry, I don't have the time.")
-  print("Goodbye.")
+    print("Sorry, I don't have the time.")
+    print("Goodbye.")
   
