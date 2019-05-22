9ca415d41ce6754ab79357266724a6dbf56b1da6
Jocelyn Lee <47438128+JJace421@users.noreply.github.com>
2019-05-22 11:05:12 -0400

    Update button.py

--- button.py
+++ button.py
@@ -1,7 +1,3 @@
-
-
-
-
 import arcade
 
 
@@ -9,15 +5,21 @@ WIDTH = 640
 HEIGHT = 480
 
 #           x   y   w   h   color                           click color
-button1 = [185,240,250,50,arcade.color.ATOMIC_TANGERINE, arcade.color.ORIOLES_ORANGE]
+button1 = [185,240,250,50,arcade.color.ORIOLES_ORANGE,arcade.color.ATOMIC_TANGERINE,False]
+button2 = [185,140,250,50,arcade.color.PEARL_AQUA,arcade.color.NAVY_BLUE,False]
+button3 = [185,340,250,50,arcade.color.VIOLET,arcade.color.LAVENDER,False]
+button4 = [185,40,250,50,arcade.color.PALE_PINK,arcade.color.PARADISE_PINK,False]
+
+
+
 btn_x = 0
 btn_y = 1
 btn_w = 2
 btn_h = 3
 btn_c = 4
 btn_cc = 5
+clicks = 6
 
-clicks = 0
 
 def setup():
     arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
@@ -32,6 +34,7 @@ def setup():
     window.on_mouse_press = on_mouse_press
     window.on_mouse_release = on_mouse_release
 
+
     arcade.run()
 
 
@@ -41,19 +44,10 @@ def update(delta_time):
 def on_draw():
     arcade.start_render()
     # Draw in here...
-
-    if clicks % 2 == 0:
-        color = button1[btn_c]
-    else:
-        color = button1[btn_cc]
-
-    arcade.draw_xywh_rectangle_filled(
-        button1[btn_x],
-        button1[btn_y],
-        button1[btn_w],
-        button1[btn_h],
-        color)
-
+    draw_button(button1)
+    draw_button(button2)
+    draw_button(button3)
+    draw_button(button4)
 
 def on_key_press(key, modifiers):
     pass
@@ -67,16 +61,54 @@ def on_mouse_press(x, y, button, modifiers):
     global clicks
 
     print(f"Clicked at ({x}, {y}).")
-    if (x > button1[btn_x] and x < button1[btn_x] + button1[btn_w] and
-        y > button1[btn_y] and y < button1[btn_y] + button1[btn_h]):
-        print("Button clicked")
-        clicks += 1
+
+    if is_clicked(x,y,button1):
+        button1[clicks] = True
+
+    if is_clicked(x,y,button2):
+        button2[clicks] = True
+
+    if is_clicked(x,y,button3):
+        button3[clicks] = True
+
+    if is_clicked(x,y,button4):
+        button4[clicks] = True
+
+
 
 def on_mouse_release(x, y, button, modifiers):
     global clicks
 
-    clicks -= 1
+    button1[clicks] = False
+    button2[clicks] = False
+    button3[clicks] = False
+    button4[clicks] = False
 
+
+def draw_button(button):
+    global clicks
+
+    if button[clicks] == True:
+        color = button[btn_c]
+    else:
+        color = button[btn_cc]
+
+    #Draw button
+    arcade.draw_xywh_rectangle_filled(
+        button[btn_x],
+        button[btn_y],
+        button[btn_w],
+        button[btn_h],
+        color)
+
+def is_clicked(x,y,button):
+
+    if (x > button[btn_x] and x < button[btn_x] + button[btn_w] and
+        y > button[btn_y] and y < button[btn_y] + button[btn_h]):
+        print("Button clicked")
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     setup()
